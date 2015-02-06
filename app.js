aae25e39037cd3df182384868fd823fa2f52012c
Jamie <jamie@campussociety.com>
2015-02-06 16:23:35 +0000

    no message

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
  * Created by jamiestreet on 29/01/2014.
  */
 var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({ port: 80 });
+var server = new PeerServer({ port: 1234 });
 
 console.log('Fast Woo Peer JS Server started successfully.');
