1d824ff5a08bfefe5322e65da9ac3aec802d4c28
Jamie <jamie@campussociety.com>
2016-01-23 00:13:37 +0000

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
  * Created by jamiestreet on 29/01/2014.
  */
 var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({ port: 1234 });
+var server = new PeerServer({ port: 80 });
 
 console.log('Fast Woo Peer JS Server started successfully.');
